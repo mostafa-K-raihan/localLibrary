d87bdcd95c436ff11f916a1991801082af86110b
Mostafa Khalid Raihan <mraihan29@gmail.com>
2018-05-21 14:22:12 +0600

    catalog urls defined

--- localLibrary/urls.py
+++ localLibrary/urls.py
@@ -22,6 +22,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/', permenant=True))
+    path('', RedirectView.as_view(url='/catalog/'))
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
